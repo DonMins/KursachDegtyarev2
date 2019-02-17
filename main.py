1bff2aefd7065df635cee210275b251154bccf0e
DonMins <maks000156@yandex.ru>
2019-02-17 20:10:38 +0400

    Дай бог работает

--- main.py
+++ main.py
@@ -127,43 +127,177 @@ def getparams():
 
 
 if __name__ == '__main__':
-    parameter_array = getparams()
-    print("Текущие параметры")
-    print("R = {0}  l = {1}  k = {2}".format(parameter_array['R'], parameter_array['l'], parameter_array['k']))
-    print("alfa = {0} c = {1} betta = {2}".format(parameter_array['alf'], parameter_array['c'], parameter_array['betta']))
-    print("P = {0}, a = {1}".format(parameter_array['P'], parameter_array['a']))
+    exit = 0
+    while(exit==0):
+        parameter_array = getparams()
+        print("Текущие параметры")
+        print("R = {0}  l = {1}  k = {2}".format(parameter_array['R'], parameter_array['l'], parameter_array['k']))
+        print("alfa = {0} c = {1} betta = {2}".format(parameter_array['alf'], parameter_array['c'], parameter_array['betta']))
+        print("P = {0}, a = {1}".format(parameter_array['P'], parameter_array['a']))
+        yes = input("Введите +, если хотите изменить параметры  иначе - ")
+        if yes.upper() == "+":
+            dataChanged[0]=1
+            while True:
+                try:
+                    R = float(input("Введите R "))
+                    if((R<=0) or R>=10):
+                        print("Вы должны ввести положительное число больше, 0 и меньше 10, попробуйте снова.")
+                    else:
+                        break
+                except ValueError:
+                    print("Вы должны ввести положительное число больше, 0 и меньше 10, попробуйте снова.")
 
-    stepr = parameter_array['R']/int(input("Введите количество шагов по R "))
-    stept = parameter_array['T']/int(input('и по T '))
-    curtime = int(input("Введите момент времени "))
-    print("Подождите, пожалуйста, выполняются вычисления...")
+            while True:
+                try:
+                    l = float(input("Введите l "))
+                    if  (l<=0 or l>=5):
+                        print("Вы должны ввести положительное число, больше 0 и меньше 5 , попробуйте снова.")
+                    else:
+                        break
+                except ValueError:
+                    print("Вы должны ввести положительное число, больше 0 и меньше 5 , попробуйте снова.")
 
-    riarr = [st for st in np.arange(0, parameter_array['R'], stepr)]
-    args =(stepr, stept, riarr)
-    t1= time.time()
+            while True:
+                try:
+                    k = float(input("Введите k "))
+                    if  ((k<=0) or (k>1)):
+                        print("Вы должны ввести положительное число, больше 0 и меньше 1 , попробуйте снова.")
+                    else:
+                        break
+                except ValueError:
+                    print("Вы должны ввести положительное число, больше 0 и меньше 1 , попробуйте снова.")
 
-    impicit = ImplicitScheme.xOy(args)
+            while True:
+                try:
+                    alfa = float(input("Введите alfa "))
+                    if((alfa<=0) or (alfa>1)):
+                        print("Вы должны ввести положительное число, больше 0 и меньше 1 , попробуйте снова.")
+                    else:
+                        break
+                except ValueError:
+                    print("Вы должны ввести положительное число, больше 0 и меньше 1 , попробуйте снова.")
 
-    print('Время работы неявной схемы' + ' {0:.2f}'.format(time.time() - t1))
+            while True:
+                try:
+                    c = float(input("Введите c "))
+                    if ((c<=0) or (c>10)):
+                        print("Вы должны ввести положительное число, больше 0 и меньше 10 , попробуйте снова.")
+                    else:
+                        break
+                except ValueError:
+                    print("Вы должны ввести положительное число, больше 0 и меньше 10 , попробуйте снова.")
 
-    t2 = time.time()
+            while True:
+                try:
+                    betta = float(input("Введите betta "))
+                    if ((betta<=0) or (betta>1)):
+                        print("Вы должны ввести положительное число, больше 0 и меньше 1 , попробуйте снова.")
+                    else:
+                        break
+                except ValueError:
+                    print("Вы должны ввести положительное число, больше 0 и меньше 1 , попробуйте снова.")
 
-    #explicit = ExplictitScheme.xOy(args)
-    print('Время работы явной схемы' + ' {0:.2f}'.format(time.time() - t2))
+            while True:
+                try:
+                    P = float(input("Введите P "))
+                    if(P<=0):
+                        print("Вы должны ввести положительное число, попробуйте снова.")
+                    else:
+                        break
+                except ValueError:
+                    print("Вы должны ввести положительное число, попробуйте снова.")
+
+            while True:
+                try:
+                    a = float(input("Введите a "))
+                    if(a <= 0):
+                        print("Вы должны ввести целое положительное число , попробуйте снова.")
+                    else:
+                        break
+                except ValueError:
+                    print("Вы должны ввести целое положительное число , попробуйте снова.")
 
 
-    y1 = [u(step,curtime, 0.01,0) for step in riarr]
-    y2 = impicit[int(curtime / stept)]
-    #y2 = explicit[int(curtime / stept)]
-    ln0, ln1 = mpl.plot(riarr,y2,riarr,y1)
-    mpl.legend((ln0, ln1), ('явная', 'Аналитическое', "Явная"),
-               title='R: {0}, l: {1}, k: {2}, alf: {3}, c: {4}, betta: {5}, P: {6}, a: {7} \n step for R : {8} \n step '
-                     'for T: {9} \n time = {10}'.format(parameter_array['R'], parameter_array['l'], parameter_array['k'],
-                                                        parameter_array['alf'], parameter_array['c'],
-                                                        parameter_array['betta'],
-                                                        parameter_array['P'], parameter_array['a'], stepr, stept, curtime))
+            parameter_array['R'] = float(R)
+            parameter_array['l'] = float(l)
+            parameter_array['k'] = float(k)
+            parameter_array['alf'] = float(alfa)
+            parameter_array['c'] = float(c)
+            parameter_array['betta'] = float(betta)
+            parameter_array['P'] = float(P)
+            parameter_array['a'] = float(a)
 
-    mpl.xlabel('Радиус')
-    mpl.ylabel('Tемпература')
-    mpl.grid()
-    mpl.show()
+            updateParameret_array(parameter_array)
+            parameter_array = getparams()
+            print("Текущие параметры")
+            print("R = {0} l = {1} k = {2}".format(parameter_array['R'], parameter_array['l'], parameter_array['k']))
+            print("alfa = {0} c = {1} betta = {2}".format(parameter_array['alf'], parameter_array['c'], parameter_array['betta']))
+            print("P = {0}, a = {1}".format(parameter_array['P'], parameter_array['a']))
+
+        while True:
+            try:
+                a = int(input("Введите количество шагов по R "))
+                if(a <= 0):
+                    print("Вы должны ввести целое положительное число , попробуйте снова.")
+                else:
+                    break
+            except ValueError:
+                print("Вы должны ввести целое положительное число , попробуйте снова.")
+
+        stepr = parameter_array['R']/a
+
+        while True:
+            try:
+                a = int(input('и по T '))
+                if(a <= 0):
+                    print("Вы должны ввести целое положительное число , попробуйте снова.")
+                else:
+                    break
+            except ValueError:
+                print("Вы должны ввести целое положительное число , попробуйте снова.")
+
+        stept = parameter_array['T']/a
+        while True:
+            try:
+                a = int(input("Введите момент времени "))
+                if(a <= 0):
+                    print("Вы должны ввести целое положительное число , попробуйте снова.")
+                else:
+                    break
+            except ValueError:
+                print("Вы должны ввести целое положительное число , попробуйте снова.")
+        curtime = a
+        print("Подождите, пожалуйста, выполняются вычисления...")
+
+        riarr = [st for st in np.arange(0, parameter_array['R'], stepr)]
+        args =(stepr, stept, riarr)
+        t1= time.time()
+
+        impicit = ImplicitScheme.xOy(args)
+
+        print('Время работы неявной схемы' + ' {0:.2f}'.format(time.time() - t1))
+
+        t2 = time.time()
+
+            #explicit = ExplictitScheme.xOy(args)
+        print('Время работы явной схемы' + ' {0:.2f}'.format(time.time() - t2))
+
+
+        y1 = [u(step,curtime, 0.01,0) for step in riarr]
+        y2 = impicit[int(curtime / stept)]
+        #y2 = explicit[int(curtime / stept)]
+        ln0, ln1 = mpl.plot(riarr,y2,riarr,y1)
+        mpl.legend((ln0, ln1), ('явная', 'Аналитическое', "Явная"),
+                   title='R: {0}, l: {1}, k: {2}, alf: {3}, c: {4}, betta: {5}, P: {6}, a: {7} \n step for R : {8} \n step '
+                         'for T: {9} \n time = {10}'.format(parameter_array['R'], parameter_array['l'], parameter_array['k'],
+                                                            parameter_array['alf'], parameter_array['c'],
+                                                            parameter_array['betta'],
+                                                            parameter_array['P'], parameter_array['a'], stepr, stept, curtime))
+
+        mpl.xlabel('Радиус')
+        mpl.ylabel('Tемпература')
+        mpl.grid()
+        mpl.show()
+        exit = int(input("Для выхода нажмите - 1 , чтобы продолжить - 0"))
+        dataChanged[0]=0
+    print("-------------------------------------------------------------------------------------")
